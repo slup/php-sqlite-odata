29c0f60645294b49276e1ff97315fb07413a7089
PetePete <PetePete@users.noreply.github.com>
2015-06-24 13:10:47 +0200

    Update Constant.class.php

--- Constant.class.php
+++ Constant.class.php
@@ -4,6 +4,7 @@ class Constant {
 			"INTEGER" => "Edm.Int32",
 			"TEXT" => "Edm.String",
 			"BOOL" => "Edm.Boolean",
+			"DATETIME" => "Edm.DateTime"
 		);
 }
 
@@ -17,7 +18,11 @@ class DataConverter {
 		
 		if ($column['type'] == 'BOOL') {
 			return DataConverter::booleanDB2OData($value);
-		} else {
+		}
+		else if($column['type'] == 'DATETIME') {
+			return DataConverter::dateTimeDB2OData($value);
+		}
+		else {
 			return $value;
 		}
 	}
@@ -26,21 +31,32 @@ class DataConverter {
 		if (!is_array($column) || !array_key_exists('type', $column)) {
 			return null;
 		}
-		
 		if ($column['type'] == 'BOOL') {
 			return DataConverter::booleanOData2DB($value);
-		} else {
+		}
+		else if($column['type'] == 'DATETIME') {
+			return DataConverter::dateTimeOData2DB($value);
+		}		
+		else {
 			return $value;
 		}
 	}
-
-    public static function booleanDB2OData($db_value) {
+	
+	public static function booleanDB2OData($db_value) {
 		return ($db_value === 1) ? "true" : "false";
-    }
+	}
+	
+	public static function dateTimeDB2OData($db_value) {
+		return gmdate('c', strtotime($db_value));
+	}
 	
 	public static function booleanOData2DB($odata_value) {
 		return ($odata_value && strtolower($odata_value) === "true") ? 1 : 0;
 	}
+	
+	public static function dateTimeOData2DB($odata_value) {
+		$dt = strtotime($odata_value);
+		return date('Y-m-d H:i:s', $dt);
+	}
 }
-
 ?>
